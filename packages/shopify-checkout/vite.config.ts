e316b6dfdbadbb806d9f85803ec3f6027e1d72e3
Nick Richmond <nick@getnacelle.com>
2021-09-20 10:43:49 -0400

    scaffold project with vite in library mode

---
+++ packages/shopify-checkout/vite.config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import { defineConfig, UserConfigExport } from 'vite';
+
+// https://vitejs.dev/config/
+export const config: UserConfigExport = {
+  plugins: [],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'NacelleShopifyCheckout',
+      fileName: (format): string => `nacelle-shopify-checkout.${format}.js`,
+      formats: ['es', 'umd', 'iife']
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {}
+      }
+    }
+  }
+};
+
+export default defineConfig(config);
