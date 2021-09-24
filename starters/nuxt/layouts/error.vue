49665f0d9eb7db8d6cc341fb81ec8e3f2e778f58
Alex Asche <alexcasche@gmail.com>
2021-09-24 10:40:02 -0400

    patch space provider test error layout

--- starters/nuxt/layouts/error.vue
+++ starters/nuxt/layouts/error.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>404</div>
+  <space-provider
+    :config="$config.nacelle"
+    :space="initialSpace"
+    :locale="$config.nacelle.locale"
+  >
+    <event-provider>
+      <cart-provider>
+        <div>Error</div>
+      </cart-provider>
+    </event-provider>
+  </space-provider>
 </template>
+
+<script>
+import { inject } from '@nuxtjs/composition-api';
+import { SpaceProvider, EventProvider, CartProvider } from '@nacelle/vue';
+
+export default {
+  components: { SpaceProvider, EventProvider, CartProvider },
+  setup() {
+    const initialSpace = inject('initialSpace');
+    return { initialSpace };
+  }
+};
+</script>
