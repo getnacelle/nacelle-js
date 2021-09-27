50eee494f1e6e7c08c902cd0e3139e1f6f79e6c6
Nick Richmond <nick@getnacelle.com>
2021-09-27 19:40:18 -0400

    colocate unit tests with the code they're testing

--- packages/shopify-checkout/jest.config.js
+++ packages/shopify-checkout/jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   },
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['node_modules', 'mocks', 'utils']
+  testPathIgnorePatterns: ['node_modules', '__tests__']
 };
