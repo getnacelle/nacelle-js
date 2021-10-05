22cf66b1d3c660c88e25be3e322ffc435700eab1
Nick Richmond <nick@getnacelle.com>
2021-10-05 18:23:08 -0400

    transpile modules

--- starters/nuxt/nuxt.config.js
+++ starters/nuxt/nuxt.config.js
@@ -119,7 +119,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['@nacelle/vue', '@nacelle/shopify-checkout']
+  },
 
   vue: {
     config: {
