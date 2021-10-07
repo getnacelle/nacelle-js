27ec99b2b25f822bdd82eb730081038a18d4f3e4
KrisQ <kquemener1@gmail.com>
2021-10-07 09:29:25 -0500

    Add generator instance (#23)

---
+++ saofile.js
@@ -0,0 +1,20 @@
+module.exports = {
+  templateDir: 'starters/nuxt',
+  actions: [
+    {
+      type: 'add',
+      // Copy and transform all files in `starters/nuxt` folder into output directory
+      files: '**'
+    },
+    {
+      type: 'move',
+      patterns: {
+        gitignore: '.gitignore'
+      }
+    }
+  ],
+
+  async completed() {
+    console.log('completed');
+  }
+};
