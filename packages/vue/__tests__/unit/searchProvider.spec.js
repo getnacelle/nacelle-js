e1a890e43805caf73e540065a26eb86b4ef6b849
KrisQ <kquemener1@gmail.com>
2021-12-07 10:12:42 -0600

    fix: conform search provider to warp2 schema

--- packages/vue/__tests__/unit/searchProvider.spec.js
+++ packages/vue/__tests__/unit/searchProvider.spec.js
@@ -3,21 +3,21 @@ import Fuse from 'fuse.js';
 describe('Simulate & test what the search provider does', () => {
   it('searches through searchData', () => {
     const searchData = [
-      { id: 1, title: 'one' },
-      { id: 2, title: 'two' },
-      { id: 3, title: 'onetwo' }
+      { id: 1, content: { title: 'one' } },
+      { id: 2, content: { title: 'two' } },
+      { id: 3, content: { title: 'onetwo' } }
     ];
     const searchOptions = {
       relevanceThreshold: 0.5,
-      keys: ['title']
+      keys: ['content.title']
     };
     const results = new Fuse(searchData, searchOptions)
       .search(String('one'))
       .filter((result) => typeof result.item !== 'undefined')
       .map((result) => result.item);
     expect(results).toEqual([
-      { id: 1, title: 'one' },
-      { id: 3, title: 'onetwo' }
+      { id: 1, content: { title: 'one' } },
+      { id: 3, content: { title: 'onetwo' } }
     ]);
   });
 });
