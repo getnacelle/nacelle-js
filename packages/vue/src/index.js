cd29b212b1c3d0d378a24db28854d248b97925db
Alex Asche <alexcasche@gmail.com>
2021-12-16 11:20:42 -0500

    chore: rm fetching product provider (#43)
    * removes fetching
    
    * feat: rm product fetching + 100% testing coverage

--- packages/vue/src/index.js
+++ packages/vue/src/index.js
@@ -26,6 +26,5 @@ export { default as SpaceProvider } from './providers/SpaceProvider';
 
 // EXPORT UTILS
 // @index('./utils/**/*.js', (f, _) => `export { default as ${_.camelCase(f.name)} } from '${f.path}';`)
-export { default as getProductOptions } from './utils/products/getProductOptions';
 export { default as getSelectedVariant } from './utils/products/getSelectedVariant';
 // @endindex
