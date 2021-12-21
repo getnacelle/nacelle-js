720cf2638986a8c04086d4fa8d5a8e9dd945383e
Alex Asche <alexcasche@gmail.com>
2021-12-21 09:20:18 -0500

    chore(react): remove providers/composables

--- packages/react/src/composables/useSpaceProvider.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { SpaceContext } from '../providers/SpaceProvider';
-
-export default function useSpaceProvider() {
-  return useContext(SpaceContext);
-}
