e5713cb8ffe64c46c6074decf0fa0653bccc121c
Alex Asche <alexcasche@gmail.com>
2022-02-03 12:07:16 -0500

    chore: simplified search

--- reference-stores/nuxt/components/search/SearchResults.vue
+++ reference-stores/nuxt/components/search/SearchResults.vue
@@ -4,24 +4,21 @@
       <p class="text-lg leading-6">
         {{ searchResults.length }} result(s) found
       </p>
-      <div class="pt-12 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
-        <search-filters :catalog="searchResults" />
-        <div class="mt-6 lg:mt-0 lg:col-span-2 xl:col-span-3">
-          <div
-            class="
-              grid grid-cols-2
-              gap-x-4 gap-y-8
-              sm:grid-cols-3 sm:gap-x-6
-              lg:grid-cols-4
-              xl:gap-x-8
-            "
-          >
-            <product-card
-              v-for="result in searchResults"
-              :key="result.nacelleEntryId"
-              :product="result"
-            />
-          </div>
+      <div class="pt-12 lg:mt-0 lg:col-span-2 xl:col-span-3">
+        <div
+          class="
+            grid grid-cols-2
+            gap-x-4 gap-y-8
+            sm:grid-cols-3 sm:gap-x-6
+            lg:grid-cols-4
+            xl:gap-x-8
+          "
+        >
+          <product-card
+            v-for="result in searchResults"
+            :key="result.nacelleEntryId"
+            :product="result"
+          />
         </div>
       </div>
     </div>
@@ -32,13 +29,11 @@
 <script>
 import { searchCatalog } from '~/utils/searchCatalog';
 
-import SearchFilters from '~/components/search/SearchFilters.vue';
 import ProductCard from '~/components/product/ProductCard.vue';
 
 export default {
   name: 'SearchResults',
   components: {
-    SearchFilters,
     ProductCard
   },
   props: {
