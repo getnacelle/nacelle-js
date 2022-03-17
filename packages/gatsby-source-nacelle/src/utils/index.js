31e96d3c3e77a245e039e37f15351450b5ac07ff
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2022-03-17 16:04:33 -0700

    feat!: Add Inference to Content Fields (#104)
    * feat!: add type inference into NacelleContent
    
    * fix: prevent naming collisions; capitilization
    
    * wip: add remote file nodes to content images
    
    * wip: remote image file link
    
    * fix: use url path to accommodate query strings
    
    * feat!: adds inference to all other content
    
    BREAKING CHANGE
    Includes setting up Remote Image Nodes for all content types
    
    Co-authored-by: Nick Richmond <nick@getnacelle.com>
    Co-authored-by: Jason Redick <jason.redick@me.com>

--- packages/gatsby-source-nacelle/src/utils/index.js
+++ packages/gatsby-source-nacelle/src/utils/index.js
@@ -1,5 +1,7 @@
 exports.cacheIsInvalid = require('./cacheIsInvalid');
+exports.capitalize = require('./capitalize');
 exports.cmsPreviewEnabled = require('./cmsPreviewEnabled');
 exports.createRemoteImageFileNode = require('./createRemoteImageFileNode');
 exports.hasBeenIndexedSinceLastBuild = require('./checkIndexTimestamp');
 exports.replaceKey = require('./replaceKey');
+exports.findNestedImages = require('./findNestedImages');
