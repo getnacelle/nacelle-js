cccda3d4524aa89948c67967f733eede0c1e9f61
Alex Asche <alexcasche@gmail.com>
2022-04-28 09:27:39 -0400

    chore: util cleanup

--- reference-stores/nuxt/utils/cachedFetch.js
+++ reference-stores/nuxt/utils/cachedFetch.js
@@ -1,19 +1,19 @@
-// import LRU from 'lru-cache';
-// import { delay } from '~/utils/delay';
+import LRU from 'lru-cache';
+import { delay } from '~/utils/delay';
 
-// const isDev = process.env.NODE_ENV !== 'development';
-// const cache = new LRU({ max: 50, max_age: 3000000 });
-// let fetchCount = 0;
+const isDev = process.env.NODE_ENV !== 'development';
+const cache = new LRU({ max: 50, max_age: 3000000 });
+let fetchCount = 0;
 
 export const cachedFetch = async ({ key, fetcher }) => {
-  // fetchCount += 1;
+  fetchCount += 1;
   let data = null;
-  // if (!isDev && fetchCount > 1) {
-  //   await delay({ duration: 5000 });
-  //   data = await cache.get(key);
-  // }
+  if (!isDev && fetchCount > 1) {
+    await delay({ duration: 5000 });
+    data = await cache.get(key);
+  }
   if (!data) data = await fetcher();
-  // if (!isDev && data) await cache.set(key, data);
+  if (!isDev && data) await cache.set(key, data);
   return {
     data
   };
