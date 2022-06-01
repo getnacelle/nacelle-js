aac40e396837fde15dbef3c8c0774cf5d9dbc964
Alex Asche <alexcasche@gmail.com>
2022-06-01 15:00:35 -0400

    feat(gatsby): scaffold gatsby reference store  (#145)
    * feat: scaffold reference store
    
    * chore: rm submodule
    
    * chore: update readme
    
    * chore: recursive resolve rich text
    
    * chore: header mega
    
    * chore: nav component
    
    * chore: cart component
    
    * chore: minor next ref fix
    
    * chore: search complete
    
    * chore: global meta
    
    * chore: rm unused service file
    
    * chore: revert package-lock flash-sale
    
    * chore: revert package-locks
    
    * chore: revert package-lock

---
+++ reference-stores/gatsby/src/pages/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Home = () => {
+  return <div className="bg-white">HOMEPAGE</div>;
+};
+
+export default Home;
---
+++ reference-stores/gatsby/src/services/contentfulUtils.js
@@ -0,0 +1,33 @@
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+export default {
+  richText: (field) =>
+    typeof field === 'string'
+      ? field
+      : documentToHtmlString(resolveRichText(field)),
+  imageUrl: (image) => `https:${image.remoteFields.file.url}`,
+  test: (field) => resolveRichText(field)
+};
+
+const resolveRichText = (entry) => {
+  if (typeof entry === 'object') {
+    if (Array.isArray(entry)) {
+      return entry
+        .filter((item) => item)
+        .map(({ content, data, marks, ...rest }) => ({
+          ...rest,
+          content: Array.isArray(content) ? resolveRichText(content) : null,
+          data: data ?? {},
+          marks: marks ?? []
+        }));
+    } else {
+      const { content, data, marks, ...rest } = entry;
+      return {
+        ...rest,
+        content: Array.isArray(content) ? resolveRichText(content) : null,
+        data: data ?? {},
+        marks: marks ?? []
+      };
+    }
+  }
+};
