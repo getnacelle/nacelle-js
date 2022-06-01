db9a1da70d375b0f41dc64e037dd8abb0a8d4b93
Alex Asche <alexcasche@gmail.com>
2022-06-01 15:33:53 -0400

    chore: minor cart fix

--- reference-stores/gatsby/src/services/shopifyCheckout.js
+++ reference-stores/gatsby/src/services/shopifyCheckout.js
@@ -2,7 +2,7 @@ import createShopifyCheckoutClient from '@nacelle/shopify-checkout';
 
 export default createShopifyCheckoutClient({
   storefrontCheckoutToken:
-    process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_CHECKOUT_TOKEN,
-  myshopifyDomain: process.env.NEXT_PUBLIC_MYSHOPIFY_DOMAIN,
-  storefrontApiVersion: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION
+    process.env.GATSBY_SHOPIFY_STOREFRONT_CHECKOUT_TOKEN,
+  myshopifyDomain: process.env.GATSBY_MYSHOPIFY_DOMAIN,
+  storefrontApiVersion: process.env.GATSBY_SHOPIFY_STOREFRONT_API_VERSION
 });
