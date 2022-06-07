7850cb478a5926092143ad57e641c7bdc007c572
Darrik <30670444+mdarrik@users.noreply.github.com>
2022-06-07 16:13:51 -0700

    feat: stringify rich text nodes

--- packages/gatsby-source-nacelle/src/utils/linkNestedContentReferences.js
+++ packages/gatsby-source-nacelle/src/utils/linkNestedContentReferences.js
@@ -9,8 +9,11 @@ function findNestedReferences(contentObject = {}, parent = null, key) {
       // delete the contentObject from the parent so that it doesn't conflict
       // with the node link
       delete parent[key];
+    } else if (contentObject[0]?._type === 'block') {
+      // if the array's entry type is block, it's a Sanity Portable (Rich) Text block, so stringify it to JSON so it's easy to fetch
+      parent[key] = JSON.stringify(contentObject);
     } else {
-      // if not a reference, look for references in each part of the array instead
+      // if not a reference & not Sanity rich text, look for references in each part of the array instead
       Object.entries(contentObject).map(([key, value]) =>
         findNestedReferences(value, contentObject, key)
       );
@@ -22,6 +25,9 @@ function findNestedReferences(contentObject = {}, parent = null, key) {
         `${key}___NODE`
       ] = `NacelleContent-${contentObject.nacelleEntryId}`;
       delete parent[key];
+      // if we've stringified  the rich text, don't keep traversing the object since it's now a string
+    } else if (contentObject.nodeType === 'document') {
+      parent[key] = JSON.stringify(contentObject);
     } else {
       // otherwise keep traversing, looking for more references
       Object.entries(contentObject).map(([key, value]) =>
