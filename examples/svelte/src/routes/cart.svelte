d5a006d23314847b222f72e2521afc2facf45e78
Alex Asche <alexcasche@gmail.com>
2022-06-13 11:13:50 -0400

    chore: pdp data fetching

--- examples/svelte/src/routes/cart.svelte
+++ examples/svelte/src/routes/cart.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { cartCount, cartSubtotal } from '~/stores/cart'
+import { lineItems, cartCount, cartSubtotal } from '~/stores/cart'
 </script>
 
 <div class="cart">
@@ -7,44 +7,46 @@
   {#if $cartCount}
     <div class="cart__contents">
       <ul class="cart__items">
-        <!-- <li
-          :key="lineItem.id"
-          class="cart__item"
-        >
-          <div class="cart__item-media">
-            <nuxt-img
-              v-if="lineItem.variant.featuredMedia"
-              :src="lineItem.variant.featuredMedia.thumbnailSrc"
-              :alt="lineItem.variant.featuredMedia.altText"
-              class="cart__item-image"
-            />
-          </div>
-          <div class="cart__item-main">
-            <h2 class="cart__item-title">
-              {{ lineItem.variant.productTitle }}
-            </h2>
-            <p class="cart__item-subtitle">{{ lineItem.variant.title }}</p>
-            <p class="cart__item-price">${{ lineItem.variant.price }}</p>
-            <p class="cart__item-quantity">
-              <strong>Quantity:</strong> {{ lineItem.quantity }}
-            </p>
-            <button
-              class="cart__item-action"
-              @click="decrementItem(lineItem.id)"
-            >
-              -
-            </button>
-            <button
-              class="cart__item-action"
-              @click="incrementItem(lineItem.id)"
-            >
-              +
-            </button>
-            <button class="cart__item-action" @click="removeItem(lineItem.id)">
-              Remove
-            </button>
-          </div>
-        </li> -->
+        {#each lineItems as lineItem (lineItem.id)}
+          <li
+            class="cart__item"
+          >
+            <div class="cart__item-media">
+              {#if lineItem.variant.featuredMedia}
+                <img
+                  src={lineItem.variant.featuredMedia.thumbnailSrc}
+                  alt={lineItem.variant.featuredMedia.altText}
+                  class="cart__item-image"
+                />
+              {/if}
+            </div>
+            <div class="cart__item-main">
+              <h2 class="cart__item-title">
+                {lineItem.variant.productTitle}
+              </h2>
+              <p class="cart__item-subtitle">{lineItem.variant.title}</p>
+              <p class="cart__item-price">${lineItem.variant.price}</p>
+              <p class="cart__item-quantity">
+                <strong>Quantity:</strong> {lineItem.quantity}
+              </p>
+              <!-- <button
+                class="cart__item-action"
+                @click="decrementItem(lineItem.id)"
+              >
+                -
+              </button>
+              <button
+                class="cart__item-action"
+                @click="incrementItem(lineItem.id)"
+              >
+                +
+              </button>
+              <button class="cart__item-action" @click="removeItem(lineItem.id)">
+                Remove
+              </button> -->
+            </div>
+          </li>
+        {/each}
       </ul>
       <p class="cart__subtotal">
         <strong>Subtotal:</strong> {$cartSubtotal}
--- examples/svelte/src/routes/products/[handle].svelte
+++ examples/svelte/src/routes/products/[handle].svelte
@@ -1,7 +1,120 @@
+<script context="module">
+  import { nacelleClient } from '~/services'
+
+  const PAGE_QUERY = `
+    query ProductPage($handle: String!){
+      products(filter: { handles: [$handle] }){
+        nacelleEntryId
+        sourceEntryId
+        content{
+          handle
+          title
+          description
+          options{
+            name
+            values
+          }
+          featuredMedia{
+            src
+            thumbnailSrc
+            altText
+          }
+        }
+        variants{
+          nacelleEntryId
+          sourceEntryId
+          sku
+          availableForSale
+          price
+          compareAtPrice
+          content{
+            title
+            selectedOptions{
+              name
+              value
+            }
+            featuredMedia{
+              src
+              thumbnailSrc
+              altText
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  export async function load({ params }) {
+    const { products } = await nacelleClient.query({
+      query: PAGE_QUERY,
+      variables: { handle: params.handle }
+    });
+    return {
+      props: { product: products[0] }
+    };
+  }
+</script>
+
 <script>
-  import { page } from '$app/stores';
+  export let product
 </script>
 
 <div>
-  {$page.params.handle}
+  {#if product}
+    <div class="product">
+      {#if product.content.featuredMedia}
+        <div class="product__media">
+          <img
+            src={product.content.featuredMedia.src}
+            alt={product.content.featuredMedia.altText}
+            class="product__image"
+          />
+        </div>
+      {/if}
+      <div class="product__main">
+        {#if product.content.title}
+          <h1 class="product__title">
+            {product.content.title}
+          </h1>
+        {/if}
+      </div>
+    </div>
+  {/if}
 </div>
+
+<style lang=scss>
+  .product {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 40px;
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+}
+.product__media,
+.product__main {
+  width: 100%;
+  overflow: hidden;
+  @media only screen and (min-width: 768px) {
+    width: 50%;
+  }
+}
+.product__image {
+  width: 100%;
+}
+.product__main {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+}
+.product__prices {
+  display: flex;
+  gap: 10px;
+}
+.product__compare {
+  text-decoration: line-through;
+}
+</style>
