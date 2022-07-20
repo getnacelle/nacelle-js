ddc4e30a74ef63023198440c2be89654c4ab70b2
Levi Whalen <levi@getnacelle.com>
2022-07-20 09:59:36 -0700

    Update graphQLClient.ts

--- examples/with-algolia/service/src/graphQLClient.ts
+++ examples/with-algolia/service/src/graphQLClient.ts
@@ -2,12 +2,12 @@ import { createClient } from '@urql/core'
 import { fetch } from 'cross-fetch'
 
 export const graphQLClient = createClient({
-  url: process.env.STOREFRONT_API_ENDPOINT as any,
+  url: process.env.NACELLE_STOREFRONT_API_ENDPOINT as any,
   fetch: fetch,
   fetchOptions: () => {
     return {
       headers: {
-        'x-nacelle-space-token': process.env.STOREFRONT_API_TOKEN as any
+        'x-nacelle-space-token': process.env.NACELLE_STOREFRONT_API_TOKEN as any
       }
     }
   }
