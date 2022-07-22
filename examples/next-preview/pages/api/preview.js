0a2af1cf1d46b44c2f9998397bd2228d3e68dab5
KrisQ <kquemener1@gmail.com>
2022-07-22 08:39:02 -0500

    fix: guidance

--- examples/next-preview/pages/api/preview.js
+++ examples/next-preview/pages/api/preview.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
+  // NOTE: You should add a check here for a Preview Mode Secret.
+  // For more information, please see https://nextjs.org/docs/advanced-features/preview-mode#securely-accessing-it-from-your-headless-cms.
+
   // Checks for preview token.
   if (!process.env.NEXT_PUBLIC_NACELLE_STOREFRONT_PREVIEW_TOKEN) {
     return res.status(401).json({ message: 'Preview Token required' });
