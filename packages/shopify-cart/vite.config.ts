49b8ae46edff4801bd0ad1b90eeb415dd9efe590
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2022-09-02 08:58:20 -0700

    feat: cleans up vite config to ship mjs (#226)
    * feat: cleans up vite config to ship mjs
    
    also updates transpile to es2018

--- packages/shopify-cart/vite.config.ts
+++ packages/shopify-cart/vite.config.ts
@@ -6,24 +6,12 @@ export const config: UserConfig = {
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src', 'client', 'index.ts'),
-      fileName: (format) => `nacelle-shopify-cart.${format}.js`,
+      fileName: 'nacelle-shopify-cart',
       formats: ['es', 'umd', 'iife'],
       name: 'NacelleShopifyCart'
     },
-    rollupOptions: {
-      treeshake: 'smallest',
-      external: ['graphql-tag'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          'graphql-tag': 'GraphqlTag'
-        }
-      }
-    },
     sourcemap: true,
-    target: 'es2015',
-    minify: 'esbuild'
+    target: 'es2018'
   },
   plugins: []
 };
