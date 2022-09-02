4da9f4394c2f0a83912f5a4defb89a383c1d672a
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2022-09-02 14:16:27 -0400

    refactor: conditional types to simplify actions (#228)

--- packages/shopify-cart/src/utils/transformNacelleLineItemToShopifyLineItem.ts
+++ packages/shopify-cart/src/utils/transformNacelleLineItemToShopifyLineItem.ts
@@ -1,13 +1,17 @@
-import {
-  NacelleCartLineItemInput,
-  NacelleCartLineItemUpdateInput
-} from '../types/cart.type';
-import { CartLineInput, CartLineUpdateInput } from '../types/shopify.type';
 import { getShopifyIdFromNacelleId } from '.';
+import type { NacelleCartLineItemUpdateInput } from '../types/cart.type';
+import type { CartLineInput, CartLineUpdateInput } from '../types/shopify.type';
 
-export default function (
-  nacelleLines: NacelleCartLineItemInput[] | NacelleCartLineItemUpdateInput[]
-): CartLineInput[] | CartLineUpdateInput[] {
+/**
+ * If type input `T` is an array of objects containing properties of `NacelleCartLineItemUpdateInput`, we'll return `CartLineUpdateInput[]`. Otherwise, we'll return `CartLineInput[]`.
+ */
+type InputOrUpdateInput<T> = T extends NacelleCartLineItemUpdateInput[]
+  ? CartLineUpdateInput[]
+  : CartLineInput[];
+
+export default function transformNacelleLineItemToShopifyLineItem<
+  T extends Partial<NacelleCartLineItemUpdateInput>[]
+>(nacelleLines: T): InputOrUpdateInput<T> {
   return nacelleLines?.map((line) => {
     const { nacelleEntryId, ...rest } = line;
     const shopifyItem = { ...rest } as CartLineInput | CartLineUpdateInput;
@@ -15,5 +19,5 @@ export default function (
       shopifyItem.merchandiseId = getShopifyIdFromNacelleId(nacelleEntryId);
     }
     return shopifyItem;
-  }) as CartLineInput[] | CartLineUpdateInput[];
+  }) as InputOrUpdateInput<T>;
 }
