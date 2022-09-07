73dd2f5fbf23a98f11faed7ab90fd8a8e49e7887
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2022-09-07 08:54:52 -0700

    feat: use src/index.ts as new entry file (#230)
    - Exports all public types from 1 file
    - Allows ts users to import type {type} from '@nacelle/shopify-checkout'

---
+++ packages/shopify-cart/src/index.ts
@@ -0,0 +1,24 @@
+/**Entry File - Serves as the public api for `@nacelle/shopify-cart`
+ * Any "public" functions and types should be exported from here
+ */
+
+// export the default export of `./client/index.ts` as the package's default export
+export { default } from './client';
+
+// export any other exports, which includes public types
+export * from './client';
+
+// export other public types
+export type {
+  Cart,
+  CartResponse,
+  NacelleCartInput,
+  NacelleCartLineItemInput,
+  NacelleCartLineItemUpdateInput
+} from './types/cart.type';
+export type { ShopifyError } from './types/errors.type';
+export type {
+  AttributeInput,
+  CartBuyerIdentityInput,
+  CartUserError
+} from './types/shopify.type';
