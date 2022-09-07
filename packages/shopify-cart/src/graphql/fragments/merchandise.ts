bf4266526aacb79d61f3df81406970f587204951
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2022-09-07 11:20:42 -0400

    refactor: custom fragments (#229)
    * refactor: replace merchandise, rather than extend it
    
    * docs: custom fragments
    
    fix: whitespace
    
    docs: add changeset
    
    * docs: simplify document structure
    
    * docs: add docs links & remove excess intro
    
    * docs: remove duplicate link
    
    * docs: remove distracting links

--- packages/shopify-cart/src/graphql/fragments/merchandise.ts
+++ packages/shopify-cart/src/graphql/fragments/merchandise.ts
@@ -1,6 +1,31 @@
 export default () => /* GraphQL */ `
   fragment Merchandise_merchandise on ProductVariant {
-    id
-    ...ProductVariant_extendCartLineMerchandise
+    availableForSale
+    compareAtPriceV2 {
+      ...Money_money
+    }
+    priceV2 {
+      ...Money_money
+    }
+    requiresShipping
+    title
+    image {
+      id
+      url
+      altText
+      width
+      height
+    }
+    product {
+      handle
+      onlineStoreUrl
+      tags
+      title
+      vendor
+    }
+    selectedOptions {
+      name
+      value
+    }
   }
 `;
