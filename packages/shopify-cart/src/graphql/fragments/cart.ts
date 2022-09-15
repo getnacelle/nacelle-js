75fe35b2e75712a35ef8a21959d3515d7f5af795
Nick Richmond <nick@getnacelle.com>
2022-09-15 13:32:05 -0400

    fix: allow merchandise fragment to be supplied

--- packages/shopify-cart/src/graphql/fragments/cart.ts
+++ packages/shopify-cart/src/graphql/fragments/cart.ts
@@ -6,12 +6,19 @@ import defaultMerchandise from './merchandise';
 import defaultMoney from './money';
 
 export interface CartFragments {
+  // NOTE: @index directives are for the 'Generate Index' VSCode Extension (jayfong.generate-index)
+  //  once the 'Generate Index' extension is installed,
+  //  open the command palette with Command+Shift+P / Ctrl+Shift+P,
+  //  then search for & select 'Generate Index'
+
+  // @index('./!(*.spec|index|userErrors).ts', (f, _) => `${_.constantCase(f.name)}?: string;`)
   BUYER_IDENTITY?: string;
   DISCOUNT_ALLOCATION?: string;
   EXTEND_CART?: string;
   EXTEND_CART_LINE?: string;
-  EXTEND_CART_LINE_MERCHANDISE?: string;
+  MERCHANDISE?: string;
   MONEY?: string;
+  // @endindex
 }
 
 export default (customFragments?: CartFragments) => /* GraphQL */ `
@@ -96,7 +103,7 @@ export default (customFragments?: CartFragments) => /* GraphQL */ `
       code
     }
   }
-  ${defaultMerchandise()}
+  ${customFragments?.MERCHANDISE ?? defaultMerchandise()}
   ${customFragments?.BUYER_IDENTITY ?? defaultBuyerIdentity()}
   ${customFragments?.DISCOUNT_ALLOCATION ?? defaultDiscountAllocation()}
   ${customFragments?.EXTEND_CART ?? defaultExtendCart()}
