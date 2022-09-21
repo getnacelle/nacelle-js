05b00be5646f167d2d0220fc22b4ecaeaad4c53e
Alex Asche <alexcasche@gmail.com>
2022-09-21 16:30:14 -0400

    chore: fix variant issue

--- packages/shopify-cart/src/utils/transformShopifyLineToNacelleLine.ts
+++ packages/shopify-cart/src/utils/transformShopifyLineToNacelleLine.ts
@@ -12,12 +12,16 @@ export default function ({
   locale
 }: TransformShopifyCartLineParams): NacelleCartLine[] {
   return lines?.map((line) => {
+    const shopifyVariantId = line.merchandise.sourceEntryId
+      .split('gid://shopify/ProductVariant/')[1]
+      .replace('/', '');
+
     return {
       ...line,
       merchandise: {
         ...line.merchandise,
         nacelleEntryId: btoa(
-          `id://SHOPIFY/${shopifyShopId}/default/PRODUCT_VARIANT/${line.merchandise.sourceEntryId}/${locale}`
+          `id://SHOPIFY/${shopifyShopId}/default/PRODUCT_VARIANT/${shopifyVariantId}/${locale}`
         )
       }
     };
