48ca0dc1dbe43cb915b0c2e732a2ef32fc37ed02
KrisQ <kquemener1@gmail.com>
2022-09-22 14:00:16 -0500

    feat: shopify-cart in nuxt starter (#249)

--- starters/nuxt/store/index.js
+++ starters/nuxt/store/index.js
@@ -1,6 +1,5 @@
 export const actions = {
   async nuxtClientInit({ dispatch }) {
-    await dispatch('checkout/initCheckout', false, { root: true });
     await dispatch('cart/initCart', false, { root: true });
   }
 };
