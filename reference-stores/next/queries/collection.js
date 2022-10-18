ca3945b27513b15333a07248b283171a8642621e
KrisQ <kquemener1@gmail.com>
2022-10-18 10:33:02 -0500

    fix: update query files

--- reference-stores/next/queries/collection.js
+++ reference-stores/next/queries/collection.js
@@ -2,9 +2,17 @@ import { PRODUCT_QUERY_FRAGMENT } from './product';
 
 export const COLLECTION_PRODUCTS_QUERY = `
   query CollectionProducts($handle: String!, $after: String!){
-    collections: productCollections(filter: { handles: [$handle] }){
-      products(first: 12, after: $after){
-        ${PRODUCT_QUERY_FRAGMENT}
+    collections: allProductCollections(filter: { handles: [$handle] }){
+      edges {
+				node {
+          products: productConnection(first: 12, after: $after){
+            pageInfo {
+              endCursor
+              hasNextPage
+            }      
+            ${PRODUCT_QUERY_FRAGMENT}
+          }
+        }
       }
     }
   }
--- reference-stores/next/queries/productPage.js
+++ reference-stores/next/queries/productPage.js
@@ -2,9 +2,13 @@ import { CONTENT_QUERY_FRAGMENT } from './content';
 
 export const PRODUCT_ROUTES_QUERY = `
 {
-  products {
-    content {
-      handle
+  products: allProducts {
+    edges {
+      node {
+        content {
+          handle
+        }
+      }
     }
   }
 }
@@ -12,45 +16,49 @@ export const PRODUCT_ROUTES_QUERY = `
 
 export const PRODUCT_PAGE_QUERY = `
   query ProductPage($handle: String!, $pageHandle: String!){
-    products: products(filter: { handles: [$handle] }){
-      nacelleEntryId
-      sourceEntryId
-      content{
-        handle
-        title
-        description
-        options{
-          name
-          values
-        }
-        media{
-          type
-          src
-          altText
-        }
-      }
-      variants{
-        nacelleEntryId
-        sourceEntryId
-        sku
-        availableForSale
-        price
-        compareAtPrice
-        content{
-          title
-          selectedOptions{
-            name
-            value
+    products: allProducts(filter: { handles: [$handle] }){
+      edges {
+        node {
+          nacelleEntryId
+          sourceEntryId
+          content{
+            handle
+            title
+            description
+            options{
+              name
+              values
+            }
+            media{
+              type
+              src
+              altText
+            }
           }
-          featuredMedia{
-            src
-            thumbnailSrc
-            altText
-          }
+          variants{
+            nacelleEntryId
+            sourceEntryId
+            sku
+            availableForSale
+            price
+            compareAtPrice
+            content{
+              title
+              selectedOptions{
+                name
+                value
+              }
+              featuredMedia{
+                src
+                thumbnailSrc
+                altText
+              }
+            }
+          }    
         }
       }
     }
-    pages: content(filter: { type: "pageProduct", handles: [$pageHandle] }){
+    pages: allContent(filter: { type: "pageProduct", handles: [$pageHandle] }){
       ${CONTENT_QUERY_FRAGMENT}
     }
   }
