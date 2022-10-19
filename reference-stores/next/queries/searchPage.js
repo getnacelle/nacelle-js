3a93ebb9773fbf692eeea7a149dae19660a53adb
KrisQ <kquemener1@gmail.com>
2022-10-19 09:55:15 -0500

    fix: format
    Co-authored-by: Nick Richmond <5732000+NWRichmond@users.noreply.github.com>

--- reference-stores/next/queries/searchPage.js
+++ reference-stores/next/queries/searchPage.js
@@ -2,7 +2,7 @@ import { PRODUCT_QUERY_FRAGMENT } from './product';
 
 export const SEARCH_PAGE_QUERY = `
   query SearchPage{
-    products: allProducts{
+    products: allProducts {
       ${PRODUCT_QUERY_FRAGMENT}
     }
   }
