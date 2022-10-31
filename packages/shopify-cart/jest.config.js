55f67852edc1bf15d1c0dc9c63dd17f9e404c7a6
KrisQ <kquemener1@gmail.com>
2022-10-31 10:15:46 -0500

    chore: add SonarCloud scan + coverage analysis

--- packages/shopify-cart/jest.config.js
+++ packages/shopify-cart/jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   preset: 'ts-jest',
-  coverageReporters: ['text', 'text-summary'],
+  coverageReporters: ['text', 'lcov'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['node_modules', '__tests__']
 };
