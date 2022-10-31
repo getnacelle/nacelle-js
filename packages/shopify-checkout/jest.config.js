395e80ac5bab1cc864b9a5c353071335b4b667e2
KrisQ <kquemener1@gmail.com>
2022-10-31 09:34:44 -0500

    chore: add SonarCloud to shopify-checkout (#284)

--- packages/shopify-checkout/jest.config.js
+++ packages/shopify-checkout/jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  coverageReporters: ['text', 'text-summary'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text', 'lcov'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['node_modules', '__tests__']
