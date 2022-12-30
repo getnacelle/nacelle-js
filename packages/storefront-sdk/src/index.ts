1db6b86d6c787f021b2e23e921be617806eb06eb
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2022-12-30 12:44:47 -0500

    feat: add basic storefront client (#306)
    * feat: add basic storefront client
    
    * refactor: prefer native private class fields

--- packages/storefront-sdk/src/index.ts
+++ packages/storefront-sdk/src/index.ts
@@ -1,7 +1,18 @@
-function placeholder(): object {
-	console.log('placeholder');
+import { StorefrontClient } from './client/index.js';
+import { errorMessages } from './utils/index.js';
 
-	return {};
+export interface StorefrontClientParams {
+	/** Nacelle Storefront GraphQL Endpoint. This can be retrieved from the Nacelle Dashboard. */
+	storefrontEndpoint: string;
+
+	/** Optional fetch implementation. If not supplied, the Storefront SDK will use `globalThis.fetch`. */
+	fetchClient?: typeof globalThis.fetch;
 }
 
-export default placeholder;
+export function Storefront(params: StorefrontClientParams) {
+	if (!params?.storefrontEndpoint) {
+		throw new Error(errorMessages.missingEndpoint);
+	}
+
+	return new StorefrontClient(params);
+}
