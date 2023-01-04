5175fec7bff5babf23841029b28dcc6dbba0047b
Darrik <30670444+mdarrik@users.noreply.github.com>
2023-01-03 16:15:59 -0800

    feat: adds v2 query method

--- packages/storefront-sdk/src/client/index.ts
+++ packages/storefront-sdk/src/client/index.ts
@@ -1,6 +1,17 @@
 import { createClient } from '@urql/core';
-import type { Client as UrqlClient } from '@urql/core';
+import type {
+	Client as UrqlClient,
+	TypedDocumentNode,
+	AnyVariables,
+	CombinedError
+} from '@urql/core';
+import type { DocumentNode } from 'graphql';
 import type { StorefrontClientParams } from '../index.js';
+
+export interface StorefrontResponse<QueryDocumentType> {
+	error?: CombinedError;
+	data?: QueryDocumentType;
+}
 
 export class StorefrontClient {
 	readonly #graphqlClient: UrqlClient;
@@ -12,8 +23,16 @@ export class StorefrontClient {
 		});
 	}
 
-	// NEW METHODS GO HERE :)
-	placeholder() {
-		return this.#graphqlClient.query('', {}).toPromise();
+	query<QData = any, QVariables extends AnyVariables = any>({
+		query,
+		variables
+	}: {
+		query: TypedDocumentNode<QData, QVariables> | DocumentNode | string;
+		variables?: QVariables | string;
+	}): Promise<StorefrontResponse<QData>> {
+		return this.#graphqlClient
+			.query(query, variables as QVariables)
+			.toPromise()
+			.then((res) => ({ data: res.data, error: res.error }));
 	}
 }
