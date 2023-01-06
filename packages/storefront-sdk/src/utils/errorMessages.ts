d415a5835f263a313abfe6ef6c4e093ce9b5b752
Nick Richmond <5732000+NWRichmond@users.noreply.github.com>
2023-01-05 19:02:30 -0500

    feat: implement the `after` method (#310)
    * feat: implement after method
    
    types: improve callback type inference
    
    docs: improve accuracy
    
    refactor: remove unnecessary assignment
    
    types: improve accuracy
    
    docs: add changeset
    
    * fix: run `applyAfter` on `query` method result

--- packages/storefront-sdk/src/utils/errorMessages.ts
+++ packages/storefront-sdk/src/utils/errorMessages.ts
@@ -1,6 +1,40 @@
-const errorMessages = {
-	missingEndpoint:
-		'@nacelle/storefront-sdk must be initialized with a `storefrontEndpoint`.'
-} as const;
+import { dataFetchingMethods } from '../utils/index.js';
 
-export default errorMessages;
+function unexpectedInputMessage(
+	/** The name of the method where the unexpected input was provided */
+	methodName: string,
+
+	/** A quick description of what the expected input is */
+	expectedInputDescription: string,
+
+	/** The actual input that was received */
+	received: unknown
+): string {
+	return `[@nacelle/storefront-sdk|${methodName} method] ${expectedInputDescription}. Received: ${JSON.stringify(
+		received
+	)}.
+	}`;
+}
+
+export const errorMessages = {
+	missingEndpoint:
+		"@nacelle/storefront-sdk must be initialized with a 'storefrontEndpoint'.",
+	afterMethodInvalid: (received: unknown) =>
+		unexpectedInputMessage(
+			'after',
+			`valid 'method' names are: ${dataFetchingMethods.join(', ')}.`,
+			received
+		),
+	afterMethodCallbackIdInvalid: (received: unknown) =>
+		unexpectedInputMessage(
+			'after',
+			'callback IDs are expected to be strings',
+			received
+		),
+	afterMethodCallbackInvalid: (received: unknown) =>
+		unexpectedInputMessage(
+			'after',
+			'provided callbacks are expected to be functions or null',
+			received
+		)
+} as const;
