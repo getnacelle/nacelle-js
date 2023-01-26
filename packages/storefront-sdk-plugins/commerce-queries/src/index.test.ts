0d61c029d1dce9d36726398e0f19c35ce91fe0f2
KrisQ <kquemener1@gmail.com>
2023-01-26 10:59:34 -0600

    feat: add `navigation` method (#321)
    * feat: navigation

--- packages/storefront-sdk-plugins/commerce-queries/src/index.test.ts
+++ packages/storefront-sdk-plugins/commerce-queries/src/index.test.ts
@@ -3,6 +3,7 @@ import commerceQueriesPlugin from './index.js';
 import { StorefrontClient } from '@nacelle/storefront-sdk';
 import getFetchPayload from '../__mocks__/utils/getFetchPayload.js';
 import SpacePropertiesResult from '../__mocks__/gql/spaceProperties.js';
+import NavigationResult from '../__mocks__/gql/navigation.js';
 
 const storefrontEndpoint =
 	'https://storefront.api.nacelle.com/graphql/v1/spaces/my-space-id';
@@ -26,6 +27,7 @@ it('does not error when composed with the `StorefrontClient` class', () => {
 
 it('adds the expected methods to the `StorefrontClient` class', () => {
 	expect(typeof client.spaceProperties).toBe('function');
+	expect(typeof client.navigation).toBe('function');
 });
 
 describe('spaceProperties', () => {
@@ -60,6 +62,43 @@ describe('spaceProperties', () => {
 			)
 		);
 		const response = await client.spaceProperties();
+		expect(response.error).toBeDefined();
+		expect(response.data).toBeUndefined();
+	});
+});
+
+describe('navigation', () => {
+	beforeEach(() => mockedFetch.mockRestore());
+
+	it('fetches `navigation` with the appropriate query', async () => {
+		mockedFetch.mockImplementationOnce(() =>
+			Promise.resolve(getFetchPayload({ data: NavigationResult }))
+		);
+		await client.navigation({ groupId: 'groupId' });
+
+		expect(mockedFetch).toHaveBeenCalledOnce();
+		expect(mockedFetch).toHaveBeenCalledWith(
+			storefrontEndpoint,
+			expect.objectContaining({
+				body: expect.stringMatching('(?=.*navigation)(?=.*groupId)') as string
+			})
+		);
+	});
+
+	it('should return the error if one of the requests errors', async () => {
+		mockedFetch.mockImplementationOnce(() =>
+			Promise.resolve(
+				getFetchPayload({
+					errors: [
+						{
+							message: 'xxx',
+							extensions: { code: 'xxx' }
+						}
+					]
+				})
+			)
+		);
+		const response = await client.navigation();
 		expect(response.error).toBeDefined();
 		expect(response.data).toBeUndefined();
 	});
