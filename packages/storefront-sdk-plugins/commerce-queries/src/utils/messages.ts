02b418f144290bf999f9abd3355f2703094870dd
Nick Richmond <nick@getnacelle.com>
2023-02-01 15:04:33 -0500

    refactor: remove unnecessary variable

--- packages/storefront-sdk-plugins/commerce-queries/src/utils/messages.ts
+++ packages/storefront-sdk-plugins/commerce-queries/src/utils/messages.ts
@@ -1,8 +1,14 @@
-import type { dataFetchingMethods } from './constants.js';
-
 export const highEntriesPerPageMessage =
 	'You have set the `entriesPerPage` option to a high value. We recommended keeping this value small for collections to avoid issues.';
 
-type DataFetchingMethod = typeof dataFetchingMethods[number];
+type DataFetchingMethod =
+	| 'content'
+	| 'navigation'
+	| 'products'
+	| 'productCollections'
+	| 'productCollectionEntries'
+	| 'query'
+	| 'spaceProperties';
+
 export const entryIdsAndHandlesMessage = (methodName: DataFetchingMethod) =>
 	`You have supplied both a nacelleEntryIds and handles. The ${methodName} method will use nacelleEntryIds for querying.`;
