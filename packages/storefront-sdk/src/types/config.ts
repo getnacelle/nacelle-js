bb5c348cc240497f0c5bf70e033c0a73f460790d
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2023-02-06 08:28:50 -0800

    feat: add advanced option to control APQ (#327)

--- packages/storefront-sdk/src/types/config.ts
+++ packages/storefront-sdk/src/types/config.ts
@@ -1,9 +1,13 @@
 import type { Maybe } from './storefront.js';
-import type { StorefrontClientParams } from '../index.js';
+import type {
+	StorefrontClientAdvancedOptions,
+	StorefrontClientParams
+} from '../index.js';
 import type { AfterSubscriptions, DataFetchingMethodName } from './after.js';
 
 export interface SetConfigParams {
 	previewToken?: Maybe<string>;
+	advancedOptions?: Maybe<StorefrontClientAdvancedOptions>;
 }
 export interface SetConfigResponse {
 	endpoint: string;
