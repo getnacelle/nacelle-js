733844465ffd822160e00279a96931939520c385
KrisQ <kquemener1@gmail.com>
2023-02-27 09:41:45 -0600

    chore: remove storefront method (#340)
    * chore: remove storefront method

--- packages/storefront-sdk/src/index.test.ts
+++ packages/storefront-sdk/src/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest';
-import { Storefront, StorefrontClient } from './index.js';
+import { StorefrontClient } from './index.js';
 import { errorMessages } from './utils/index.js';
 import type { StorefrontClientParams } from './index.js';
 
@@ -7,17 +7,17 @@ const storefrontEndpoint =
 	'https://storefront.api.nacelle.com/graphql/v1/spaces/my-space-id';
 
 it('throws an error if the client is initialized without required parameters', () => {
-	expect(() => Storefront({} as StorefrontClientParams)).toThrowError(
+	expect(() => new StorefrontClient({} as StorefrontClientParams)).toThrowError(
 		errorMessages.missingEndpoint
 	);
 });
 
 it('can be correctly initialized when the required parameters are supplied', () => {
-	expect(() => Storefront({ storefrontEndpoint })).not.toThrow();
+	expect(() => new StorefrontClient({ storefrontEndpoint })).not.toThrow();
 });
 
 it('returns a `StorefrontClient` instance', () => {
-	const client = Storefront({ storefrontEndpoint });
+	const client = new StorefrontClient({ storefrontEndpoint });
 
 	expect(client).toBeInstanceOf(StorefrontClient);
 });
