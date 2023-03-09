58b4936f2a8dba14412835a37e0d5a2e06f5e13e
Nick Richmond <nick@getnacelle.com>
2023-03-08 23:20:43 -0500

    refactor: remove unnecessary ESLint comment

--- packages/storefront-sdk/src/types/after.ts
+++ packages/storefront-sdk/src/types/after.ts
@@ -1,6 +1,5 @@
 export type AfterCallback<T> = (data: T) => T;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AfterCallbackWithId<T> = {
 	[id: string]: AfterCallback<T>;
 };
