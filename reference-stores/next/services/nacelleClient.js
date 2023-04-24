f445f88ed89a868c30a6ffda835171cff40ba77e
KrisQ <kquemener1@gmail.com>
2023-04-24 10:11:44 -0500

    fix: remove unused commerce query plugin

--- reference-stores/next/services/nacelleClient.js
+++ reference-stores/next/services/nacelleClient.js
@@ -1,8 +1,5 @@
 import { StorefrontClient } from '@nacelle/storefront-sdk';
-import { CommerceQueries } from '@nacelle/commerce-queries-plugin';
 
-const ClientWithCommerceQueries = CommerceQueries(StorefrontClient);
-
-export default new ClientWithCommerceQueries({
+export default new StorefrontClient({
   storefrontEndpoint: process.env.NEXT_PUBLIC_NACELLE_STOREFRONT_ENDPOINT
 });
