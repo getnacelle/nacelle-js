b0a8924724a14753a49d68b8600ced70b53664d6
Nick Richmond <nick@getnacelle.com>
2023-10-19 14:30:30 -0400

    fix: urql syntax and dependencies

--- examples/with-algolia/service/src/graphQLClient.ts
+++ examples/with-algolia/service/src/graphQLClient.ts
@@ -1,14 +1,16 @@
-import { createClient } from '@urql/core'
-import { fetch } from 'cross-fetch'
+import { createClient, cacheExchange, fetchExchange } from '@urql/core';
+import { fetch } from 'cross-fetch';
 
 export const graphQLClient = createClient({
-  url: process.env.NACELLE_STOREFRONT_API_ENDPOINT as any,
-  fetch: fetch,
+  url: process.env.NACELLE_STOREFRONT_API_ENDPOINT as string,
+  exchanges: [cacheExchange, fetchExchange],
+  fetch,
   fetchOptions: () => {
     return {
       headers: {
-        'x-nacelle-space-token': process.env.NACELLE_STOREFRONT_API_TOKEN as any
+        'x-nacelle-space-token': process.env
+          .NACELLE_STOREFRONT_API_TOKEN as string
       }
-    }
+    };
   }
-})
+});
--- examples/with-algolia/service/src/index.ts
+++ examples/with-algolia/service/src/index.ts
@@ -1,25 +1,26 @@
-import { Product, ProductEdge } from '@nacelle/storefront-sdk'
-import { graphQLClient } from './graphQLClient'
-import { algoliaClient } from './algoliaClient'
-import { PRODUCTS_QUERY } from './graphql/PRODUCTS_QUERY'
+import { type Product, type ProductEdge } from '@nacelle/storefront-sdk';
+import { graphQLClient } from './graphQLClient';
+import { algoliaClient } from './algoliaClient';
+import { PRODUCTS_QUERY } from './graphql/PRODUCTS_QUERY';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const indexData = async () => {
   // Get products from Nacelle's Storefront API
   const products = await graphQLClient
-    .query(PRODUCTS_QUERY)
+    .query(PRODUCTS_QUERY, {})
     .toPromise()
     .then((res) =>
       res.data.allProducts.edges.map((edge: ProductEdge) => edge.node)
-    )
+    );
 
   // Format Product data for Algolia
   const productsToIndex = products.map((product: Product) => {
     return {
       objectID: product.sourceEntryId,
       ...product
-    }
-  })
+    };
+  });
 
   // Index products in Algolia
-  return algoliaClient.initIndex('nacelle').saveObjects(productsToIndex)
-}
+  algoliaClient.initIndex('nacelle').saveObjects(productsToIndex);
+};
