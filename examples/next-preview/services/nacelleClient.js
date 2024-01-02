9bca06564726aacfdfdef27e56178097b2ce8dd7
Nick Richmond <nick@getnacelle.com>
2024-01-02 11:02:04 -0500

    chore: upgrade packages

--- examples/next-preview/services/nacelleClient.js
+++ examples/next-preview/services/nacelleClient.js
@@ -1,10 +1,14 @@
-import Storefront from '@nacelle/storefront-sdk';
+import { StorefrontClient } from '@nacelle/storefront-sdk';
+import { CommerceQueries } from '@nacelle/commerce-queries-plugin';
+
+const ClientWithCommerceQueries = CommerceQueries(StorefrontClient);
 
 // Initializes a Storefront client from `@nacelle/storefront-sdk`
 // using credentials provided from the Nacelle Dashboard.
-// (https://nacelle.com/docs/querying-data/storefront-sdk)
+// (https://docs.nacelle.com/docs/javascript-storefront-sdk)
 
-export default new Storefront({
-  storefrontEndpoint: process.env.NEXT_PUBLIC_NACELLE_STOREFRONT_ENDPOINT,
-  token: process.env.NEXT_PUBLIC_NACELLE_STOREFRONT_TOKEN
+const nacelleClient = new ClientWithCommerceQueries({
+  storefrontEndpoint: process.env.NEXT_PUBLIC_NACELLE_STOREFRONT_ENDPOINT
 });
+
+export default nacelleClient;
